3c6b133512a20c75adb939e0a774f64f712263cb
vlad2102 <matysiukv@gmail.com>
2023-02-10 12:09:02 +0100

    Some swagger changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Main application module"""
-
+import connexion
 from flask import Flask
 
 from config import get_environment_config_object
@@ -12,28 +12,38 @@ from routes.user_route import user_control, user_manipulation
 
 def create_app() -> Flask:
     """Factory method for Flask provider"""
-    app = Flask(__name__)
-    app.config.from_object(get_environment_config_object())
 
-    db.init_app(app)
-    ma.init_app(app)
+    connexion_app = connexion.App(
+        "__name__", specification_dir="./openapi/", options={'swagger_ui': True},
+    )
+    connexion_app.add_api("swagger.yml")
 
-    with app.app_context():
+    application = connexion_app.app
+    application.config.from_object(get_environment_config_object())
+
+    db.init_app(application)
+    ma.init_app(application)
+
+    with application.app_context():
         db.create_all()
-        return app
+        return application
 
 
-def add_routes(app: Flask) -> None:
+def add_routes(application: Flask) -> None:
     """Add routes for objects management purposes"""
 
-    app.add_url_rule("/booking", methods=['GET', 'POST'], view_func=booking_control)
-    app.add_url_rule("/booking/<int:booking_id>", methods=['GET', 'PUT', 'DELETE'], view_func=booking_manipulation)
+    application.add_url_rule("/booking", methods=['GET', 'POST'], view_func=booking_control)
+    application.add_url_rule(
+        "/booking/<int:booking_id>", methods=['GET', 'PUT', 'DELETE'], view_func=booking_manipulation
+    )
 
-    app.add_url_rule("/product", methods=['GET', 'POST'], view_func=product_control)
-    app.add_url_rule("/product/<int:product_id>", methods=['GET', 'PUT', 'DELETE'], view_func=product_manipulation)
+    application.add_url_rule("/product", methods=['GET', 'POST'], view_func=product_control)
+    application.add_url_rule(
+        "/product/<int:product_id>", methods=['GET', 'PUT', 'DELETE'], view_func=product_manipulation
+    )
 
-    app.add_url_rule("/user", methods=['GET', 'POST'], view_func=user_control)
-    app.add_url_rule("/user/<int:user_id>", methods=['GET', 'PUT', 'DELETE'], view_func=user_manipulation)
+    application.add_url_rule("/user", methods=['GET', 'POST'], view_func=user_control)
+    application.add_url_rule("/user/<int:user_id>", methods=['GET', 'PUT', 'DELETE'], view_func=user_manipulation)
 
 
 app = create_app()
